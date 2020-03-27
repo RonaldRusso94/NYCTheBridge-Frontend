8d941b96ac07638889ffa76bb7271ad038a2205d
Ronald Russo <RonaldRusso94@yahoo.com>
2020-03-27 17:47:07 -0400

    Update GenreItem.js

--- src/components/genres/GenreItem.js
+++ src/components/genres/GenreItem.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router-dom';
 const GenreItem = ({ genre: { genre, img } }) => {
   return (
     <div className='card text-center py-2'>
-      {/* <Link to={`/album/${albumId}`}>
-        <img src={albumImg} alt='' style={{ width: '80%' }} />
-      </Link> */}
       <Link to={`/genre/${genre}`}>
         <img src={img} style={{ width: '80%' }} alt='' />
         <h3>{genre}</h3>
