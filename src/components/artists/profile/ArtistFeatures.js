14e011dc44bbc8ea2cc881d629c45945762ba6ab
Ronald Russo <RonaldRusso94@yahoo.com>
2020-05-15 19:22:57 -0400

    ArtistFeatures.js cleanup

--- src/components/artists/profile/ArtistFeatures.js
+++ src/components/artists/profile/ArtistFeatures.js
@@ -19,32 +19,17 @@ const ArtistFeatures = ({ paramsId }) => {
 
   albumsContext.featuredOn.forEach((feature) => {
     featuredOnArr.push(feature);
-    // console.log('arrAlbum', featuredOnArr);
   });
-  // console.log('ALBUM FINISHED');
 
   singlesContext.featuredOn.forEach((feature) => {
     featuredOnArr.push(feature);
-    // console.log('arrSingle', featuredOnArr);
   });
-
-  // console.log('SINGLE FINISHED', featuredOnArr);
-
-  // featuredOnArr.sort(function (a, b) {
-  //   var dateA = new Date(a.date),
-  //     dateB = new Date(b.date);
-  //   return dateB - dateA;
-  // });
 
   featuredOnArr
     .sort(function (a, b) {
       return new Date(a.date) - new Date(b.date);
     })
     .reverse();
-
-  // featuredOnArr.sort(function (a, b) {
-  //   return a.date < b.date ? -1 : a.date > b.date ? 1 : 0;
-  // });
 
   console.log('AFTER', featuredOnArr);
 
@@ -57,18 +42,6 @@ const ArtistFeatures = ({ paramsId }) => {
         <h3>Appears On</h3>
         <hr className='mb-1' />
 
-        {/* {albumsContext.featuredOn.length > 0 &&
-          albumsContext.featuredOn.map((album) => {
-            return (
-              <div key={album._id} className='grid-4'>
-                <div>
-                  <img src={album.img} alt='' />
-                  <h5>{album.title}</h5>
-                  <h5>{album.date.replace(/-.+/, ' ')}</h5>
-                </div>
-              </div>
-            );
-          })} */}
         <div className='grid-4'>
           {featuredOnArr.map((item) => {
             return (
@@ -80,18 +53,6 @@ const ArtistFeatures = ({ paramsId }) => {
             );
           })}
         </div>
-        {/* <div className='grid-4'>
-          {singlesContext.featuredOn.length > 0 &&
-            singlesContext.featuredOn.map((single) => {
-              return (
-                <div key={single._id}>
-                  <img src={single.img} alt='' />
-                  <h5>{single.title}</h5>
-                  <h5>{single.date.replace(/-.+/, ' ')}</h5>
-                </div>
-              );
-            })}
-        </div> */}
       </>
     );
   } else {
