a729333ac9b2117bac03557ecccb334304cbf738
Ronald Russo <RonaldRusso94@yahoo.com>
2020-05-15 19:20:23 -0400

    Update ArtistFeatures.js regex to format iso dates & sort by most recent

--- src/components/artists/profile/ArtistFeatures.js
+++ src/components/artists/profile/ArtistFeatures.js
@@ -15,6 +15,39 @@ const ArtistFeatures = ({ paramsId }) => {
     // eslint-disable-next-line
   }, []);
 
+  const featuredOnArr = [];
+
+  albumsContext.featuredOn.forEach((feature) => {
+    featuredOnArr.push(feature);
+    // console.log('arrAlbum', featuredOnArr);
+  });
+  // console.log('ALBUM FINISHED');
+
+  singlesContext.featuredOn.forEach((feature) => {
+    featuredOnArr.push(feature);
+    // console.log('arrSingle', featuredOnArr);
+  });
+
+  // console.log('SINGLE FINISHED', featuredOnArr);
+
+  // featuredOnArr.sort(function (a, b) {
+  //   var dateA = new Date(a.date),
+  //     dateB = new Date(b.date);
+  //   return dateB - dateA;
+  // });
+
+  featuredOnArr
+    .sort(function (a, b) {
+      return new Date(a.date) - new Date(b.date);
+    })
+    .reverse();
+
+  // featuredOnArr.sort(function (a, b) {
+  //   return a.date < b.date ? -1 : a.date > b.date ? 1 : 0;
+  // });
+
+  console.log('AFTER', featuredOnArr);
+
   if (
     albumsContext.featuredOn.length > 0 ||
     singlesContext.featuredOn.length > 0
@@ -22,38 +55,43 @@ const ArtistFeatures = ({ paramsId }) => {
     return (
       <>
         <h3>Appears On</h3>
+        <hr className='mb-1' />
 
-        {albumsContext.featuredOn.length > 0 &&
+        {/* {albumsContext.featuredOn.length > 0 &&
           albumsContext.featuredOn.map((album) => {
             return (
-              <div className='card'>
-                <img
-                  src={album.img}
-                  alt=''
-                  style={{ height: '10%', width: '10%' }}
-                />
-                <h5>
-                  {album.title} - {album.artist}
-                </h5>
+              <div key={album._id} className='grid-4'>
+                <div>
+                  <img src={album.img} alt='' />
+                  <h5>{album.title}</h5>
+                  <h5>{album.date.replace(/-.+/, ' ')}</h5>
+                </div>
               </div>
             );
-          })}
-
-        {singlesContext.featuredOn.length > 0 &&
-          singlesContext.featuredOn.map((single) => {
+          })} */}
+        <div className='grid-4'>
+          {featuredOnArr.map((item) => {
             return (
-              <div className='card'>
-                <img
-                  src={single.img}
-                  alt=''
-                  style={{ height: '10%', width: '10%' }}
-                />
-                <h5>
-                  {single.title} - {single.artist}
-                </h5>
+              <div key={item._id}>
+                <img src={item.img} alt='' />
+                <h5>{item.title}</h5>
+                <h5>{item.date.replace(/-.+/, ' ')}</h5>
               </div>
             );
           })}
+        </div>
+        {/* <div className='grid-4'>
+          {singlesContext.featuredOn.length > 0 &&
+            singlesContext.featuredOn.map((single) => {
+              return (
+                <div key={single._id}>
+                  <img src={single.img} alt='' />
+                  <h5>{single.title}</h5>
+                  <h5>{single.date.replace(/-.+/, ' ')}</h5>
+                </div>
+              );
+            })}
+        </div> */}
       </>
     );
   } else {
